9a4fa9f8ee82174a3baa8ee2d41ecc41630b2cd9
Mihail Parvanov <mihail.parvanov@telerik.com>
2019-01-21 13:31:00 +0200

    Update the application in accrdance with 5-components-events-props task

---
+++ work/app.1.js
@@ -0,0 +1,31 @@
+var House = {
+    template: '#house-template',
+    props: {
+      house: Object,
+      houseId: String,
+      currentHouse: String
+    },
+    computed: {
+      isVisible () {
+        return this.currentHouse === this.houseId
+      }
+    }
+  }
+
+
+var app = new Vue({
+    el: '#app',
+    components: {
+      House: House
+    },
+    data: {
+      houses: houses,
+      currentHouse: null
+    },
+    methods: {
+      showHouse (houseId) {
+        this.currentHouse = houseId
+      }
+    
+    }
+})
